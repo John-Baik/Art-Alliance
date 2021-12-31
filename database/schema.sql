4fe96953d3d29923ba48bbb2292501dca0d693e2
John Baik <johnwbaik@gmail.com>
2021-12-30 22:32:56 -0800

    Completed feature #1

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,40 @@
 set client_min_messages to warning;
-
 -- DANGER: this is NOT how to do it in the real world.
 -- `drop schema` INSTANTLY ERASES EVERYTHING.
 drop schema "public" cascade;
-
 create schema "public";
+CREATE TABLE "users" (
+    "userId" serial NOT NULL,
+    "username" TEXT NOT NULL unique,
+    "password" TEXT NOT NULL,
+    CONSTRAINT "users_pk" PRIMARY KEY ("userId")
+) WITH (
+  OIDS=FALSE
+);
+CREATE TABLE "posts" (
+    "postId" serial NOT NULL,
+    "userId" integer NOT NULL,
+    "post" TEXT NOT NULL,
+    "price" DECIMAL NOT NULL,
+    "startTime" TIME NOT NULL,
+    "endTime" TIME NOT NULL,
+    "location" TEXT NOT NULL,
+    "createdAt" timestamptz NOT NULL default now(),
+    "startDate" DATE NOT NULL,
+    CONSTRAINT "posts_pk" PRIMARY KEY ("postId")
+) WITH (
+  OIDS=FALSE
+);
+CREATE TABLE "comments" (
+    "commentId" serial NOT NULL,
+    "postId" integer NOT NULL,
+    "userId" integer NOT NULL,
+    "commentText" text NOT NULL,
+    "createdAt" timestamptz NOT NULL default now(),
+    CONSTRAINT "comments_pk" PRIMARY KEY ("commentId")
+) WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "posts" ADD CONSTRAINT "posts_fk0" FOREIGN KEY ("userId") REFERENCES "users"("userId");
+ALTER TABLE "comments" ADD CONSTRAINT "comments_fk0" FOREIGN KEY ("postId") REFERENCES "posts"("postId");
+ALTER TABLE "comments" ADD CONSTRAINT "comments_fk1" FOREIGN KEY ("userId") REFERENCES "users"("userId");
--- server/index.js
+++ server/index.js
@@ -1,13 +1,44 @@
 require('dotenv/config');
+const pg = require('pg');
 const express = require('express');
 const errorMiddleware = require('./error-middleware');
 const staticMiddleware = require('./static-middleware');
 
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+const jsonMiddleWare = express.json();
 const app = express();
 
 app.use(staticMiddleware);
 
+app.use(jsonMiddleWare);
+
 app.use(errorMiddleware);
+
+app.post('/api/posts', (req, res) => {
+  const body = req.body;
+  const userId = 1;
+  const sql = `
+  insert into "posts" ("post", "price", "startDate", "startTime", "endTime", "location", "userId")
+  values ($1, $2, $3, $4, $5, $6, $7)
+  returning *
+  `;
+  const values = [body.post, body.price, body.startDate, body.startTime, body.endTime, body.location, userId];
+  db.query(sql, values)
+    .then(result => {
+      const post = result.rows[0];
+      res.status(201).json(post);
+    })
+    .catch(error => {
+      // eslint-disable-next-line no-console
+      console.log(error);
+      res.status(500).json({ error: 'An unexpected error has occurred' });
+    });
+});
 
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
