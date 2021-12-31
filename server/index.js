809600e34aeadfeaf7c943c1e680e6e1c2892281
John Baik <johnwbaik@gmail.com>
2021-12-31 10:54:08 -0800

    Create endpoint GET /api/posts to get all available posts #2

--- server/index.js
+++ server/index.js
@@ -40,6 +40,16 @@ app.post('/api/posts', (req, res) => {
     });
 });
 
+app.get('/api/posts', (req, res, next) => {
+  const sql = `
+  select *
+  from "posts"
+  `;
+  db.query(sql)
+    .then(result => res.json(result.rows))
+    .catch(err => next(err));
+});
+
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`express server listening on port ${process.env.PORT}`);
