89f452eb50cdf0dc34b63535e36b034d687cf29b
John Baik <johnwbaik@gmail.com>
2022-01-03 21:22:05 -0800

    Create endpoint PUT /api/posts/id to retrieve individual post #3

--- server/index.js
+++ server/index.js
@@ -51,6 +51,50 @@ app.get('/api/posts', (req, res, next) => {
     .catch(err => next(err));
 });
 
+app.put('/api/posts/:postId', (req, res) => {
+  const body = req.body;
+  const id = Number(req.params.postId);
+  const post = body.post;
+  const price = body.price;
+  const startTime = body.startTime;
+  const endTime = body.endTime;
+  const location = body.location;
+  const startDate = body.startDate;
+  if (!id || id < 0) {
+    res.status(400).json({ error: 'invalid id' });
+    return;
+  } else if (!post || !price || !startTime || !endTime || !location || !startDate) {
+    res.status(400).json({ error: 'Entry must contain a price, startTime, endTime, and location' });
+    return;
+  }
+  const sql = `
+    update "posts"
+      set "post" = $1,
+      "price" = $2,
+      "startTime" = $3,
+      "endTime" = $4,
+      "location" = $5,
+      "startDate" = $6
+    where "postId" = $7
+    returning  "postId", "userId", "post", "price", "startTime", "endTime", "location", "createdAt", "startDate"
+  `;
+  const values = [post, price, startTime, endTime, location, startDate, id];
+  db.query(sql, values)
+    .then(result => {
+      const updatedPost = result.rows[0];
+      if (!updatedPost) {
+        res.status(404).json({ error: 'postId does not exist' });
+        return;
+      }
+      res.status(200).json(updatedPost);
+    })
+    .catch(error => {
+      // eslint-disable-next-line no-console
+      console.log(error);
+      res.status(500).json({ error: 'An unexpected error has occurred' });
+    });
+});
+
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`express server listening on port ${process.env.PORT}`);
